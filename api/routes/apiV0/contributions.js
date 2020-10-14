3c388747b17ffe77cfa5f4608d77b1aa87334acc
Brice Vandeputte <brice.vandeputte@orange.com>
2020-10-14 13:55:37 +0200

    Fix #3 add /api/v0/contributions and tests

--- api/routes/apiV0/contributions.js
+++ api/routes/apiV0/contributions.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const common = require('../lib/CommonService');
+const common = require('./../../lib/CommonService');
 
 router.get('/count', function(req, res, next) {
   common.assumeJd((err, jd) => {
@@ -8,7 +8,7 @@ router.get('/count', function(req, res, next) {
          common.respondError(res,err);
          return;
       }
-      jd.count((err,nb) => {
+      jd.contribsCount((err,nb) => {
       if (err) {
          common.respondError(res,err);
          return;
@@ -22,13 +22,12 @@ router.get('/names', function(req, res, next) {
   var limit = common.isNormalInteger(req.params['limit']) ? common.intValue(req.params['limit']) : null;
   var champs = ["nom"];
   var bookmark = req.params['bookmark'] ? req.params['bookmark'] : null;
-
   common.assumeJd((err, jd) => {
       if (err) {
          common.respondError(res,err);
          return;
       }
-      jd.listDocuments({"champs":champs,"limit":limit,"bookmark":bookmark}, (err,docs) => {
+      jd.listContribs({"champs":champs,"limit":limit,"bookmark":bookmark}, (err,docs) => {
       if (err) {
          common.respondError(res,err);
          return;
--- api/tests/routes.test.js
+++ api/tests/routes.test.js
@@ -7,6 +7,10 @@ const CommonService = require('../lib/CommonService')
 chai.should();
 chai.use(chaiHttp);
 
+const API_V0 = '/api/v0';
+const API_V0_DOCS = API_V0+'/docs';
+const API_V0_CONTRIBUTIONS = API_V0+'/contributions';
+
 let server;
 let docCount;
 
@@ -14,7 +18,7 @@ function _assumeSuccess(res) {
   if (res.status && (res.status < 200 || res.status > 299)) {
     console.info("res:",res.body);
   }
-  res.should.have.status(200);
+  res.status.should.be.within(200, 299, 'response status 2xx success expected');
 }
 
 function _setTestMode() {
@@ -24,6 +28,49 @@ function _setTestMode() {
     console.info("TEST MODE uri:", CommonService.MONGO_URI, " admin:", CommonService.MONGO_ADMIN_DB);
 }
 
+function _testCountEndpoint(endpoint, expectedCountResult = null) {
+  var countEndpoint = endpoint + '/count';
+  it('should get ' + countEndpoint, function(done) {
+
+      chai.request(server)
+        .get(countEndpoint)
+        .set('Accept', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          _assumeSuccess(res);
+          res.body.should.have.property('count');
+          docCount = res.body.count;
+          console.info(' * ', countEndpoint, docCount);
+          if (expectedCountResult !== null) {
+            res.body.count.should.be.eql(expectedCountResult);
+          }
+          done();
+        });
+
+  })
+}
+
+function _testNamesEndpoint(endpoint, expectedCountResult = null) {
+  var namesEndpoint = endpoint + '/names';
+  it('should get ' + namesEndpoint, function(done) {
+
+      chai.request(server)
+        .get(namesEndpoint)
+        .set('Accept', 'application/json; charset=utf-8')
+        .end((err, res) => {
+          _assumeSuccess(res);
+          res.body.should.be.a('array');
+          if (expectedCountResult !== null) {
+            res.body.length.should.be.eql(expectedCountResult);
+          }
+          console.info(" * docs names: ", res.body.map((d)=>d.nom).join(' - '));
+          done();
+        });
+
+  });
+}
+
+var endpoints = [API_V0_CONTRIBUTIONS, API_V0_DOCS];
+
 describe('server endpoints', () => {
   before(function (done) {
       _setTestMode();
@@ -31,35 +78,10 @@ describe('server endpoints', () => {
       done();
   });
 
-  it('should get /api/docs/count', function(done) {
-
-      chai.request(server)
-        .get('/api/docs/count')
-        .set('Accept', 'application/json; charset=utf-8')
-        .end((err, res) => {
-          _assumeSuccess(res);
-          res.body.should.have.property('count');
-          docCount = res.body.count;
-          // res.body.count.should.be.eql(1);
-          console.info(" * docCount: ", docCount);
-          done();
-        });
-
-  })
-
-  it('should get /api/docs/names', function(done) {
-
-      chai.request(server)
-        .get('/api/docs/names')
-        .set('Accept', 'application/json; charset=utf-8')
-        .end((err, res) => {
-          _assumeSuccess(res);
-          res.body.should.be.a('array');
-          res.body.length.should.be.eql(docCount);
-          console.info(" * docs names: ", res.body.map((d)=>d.nom).join(' - '));
-          done();
-        });
-  })
+  endpoints.forEach(endpoint => {
+    _testCountEndpoint(endpoint);
+    _testNamesEndpoint(endpoint);
+  });
 
   after(function (done) {
       server.quit();
