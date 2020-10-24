ebce93e48548ec57a5c2ad1105e2dea47d3c75e1
Brice Vandeputte <brice.vandeputte@orange.com>
2020-10-24 15:47:59 +0200

    Fix #17 about as user

--- env/initEnv.example.sh
+++ env/initEnv.example.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # create mongo db free plan from https://cloud.mongodb.com/
 # export JARDI_MONGO_URI="mongodb+srv://userHere:passwordHere@clusterHere.xzryx.mongodb.net/dbNameHere?retryWrites=true&w=majority"
-JARDI_MONGO_URI=mongodb://127.0.0.1:27017/jardin?retryWrites=true&w=majority
-# export JARDI_ADMIN_MONGO_DB_NAME=jardinAdmin
+export JARDI_MONGO_URI="mongodb://127.0.0.1:27017/jardin?retryWrites=true&w=majority"
+export JARDI_ADMIN_MONGO_DB_NAME=jardinAdmin
 
 # test
 #export JARDI_TEST_MONGO_URI="mongodb://127.0.0.1:27017/test-jardin?retryWrites=true&w=majority"
