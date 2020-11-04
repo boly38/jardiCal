927c03c4725d18cbbf724744f2e6d9621539ff9a
Brice Vandeputte <brice.vandeputte@orange.com>
2020-11-04 20:29:37 +0100

    Fix #29 add to jard-lib 1.0.7 with Promise
    - add travis mongo wait

--- routes/apiV0/about.js
+++ routes/apiV0/about.js
@@ -6,48 +6,25 @@ const common = require('./../../lib/CommonService');
 
 router.get('/', function(req, res, next) {
 
-  common.assumeJd((err, jd) => {
-    if (err) {
-       common.respondError(res,err);
-       return;
-    }
+  common.jdResponse(res, async function(jd) {
     var jdConfig = jd.getConfiguration();
+    var aboutResult = {
+        "version":{
+          "api": pjson.version,
+          "jardi-lib": jdConfig.version
+        },
+        "roles": jdConfig.roles,
+    }
 
-    async.waterfall(
-        [
-            function(callback) {
-                jd.count(callback);
-            },
-            function(docsCount, callback) {
-              if (jdConfig.roles.includes('admin')) {
-                jd.contribsCount((err, contribsCount) => callback(err, {docsCount,contribsCount}));
-              } else {
-                callback(err, {docsCount});
-              }
-            }
-        ],
-        function (err, counts) {
-            var aboutResult = {
-                "version":{
-                  "api": pjson.version,
-                  "jardi-lib": jdConfig.version
-                },
-                "roles": jdConfig.roles,
-            }
-            if (err) {
-                aboutResult.error = "about can't retrieve counts : " + JSON.stringify(err);
-            } else {
-                aboutResult.dbName = jdConfig.db.name;
-                aboutResult.documents = counts.docsCount;
-                if (jdConfig.roles.includes('admin')) {
-                  aboutResult.adminDbName = jdConfig.adminDb.name;
-                  aboutResult.contributions = counts.contribsCount;
-                }
-            }
-            res.send(aboutResult);
-        }
-    );
-
+    var docsCount = await jd.count();
+    aboutResult.dbName = jdConfig.db.name;
+    aboutResult.documents = docsCount;
+    if (jdConfig.roles.includes('admin')) {
+      var contribsCount = await jd.contribsCount();
+      aboutResult.adminDbName = jdConfig.adminDb.name;
+      aboutResult.contributions = contribsCount;
+    }
+    res.send(aboutResult);
   });
 
 });
